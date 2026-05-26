0d0d4f695be4b3aeea043cf4548d5efdba8f6e59
chen_1335 <3388508783@qq.com>
2026-05-26 23:27:14 +0800

    热解炉配方初步

--- src/main/java/com/chen1335/immersiveMechanical/definitions/IMRecipe.java
+++ src/main/java/com/chen1335/immersiveMechanical/definitions/IMRecipe.java
@@ -1,6 +1,7 @@
 package com.chen1335.immersiveMechanical.definitions;
 
 import com.chen1335.immersiveMechanical.recipe.IndustrialFurnaceRecipe;
+import com.chen1335.immersiveMechanical.recipe.PyrolyseOvenRecipe;
 import com.chen1335.registrate.IERecipeEntry;
 
 import static com.chen1335.immersiveMechanical.ImmersiveMechanical.REGISTRATE;
@@ -12,6 +13,11 @@ public class IMRecipe {
             IndustrialFurnaceRecipe.Serializer::new
     ).register();
 
+    public static final IERecipeEntry<PyrolyseOvenRecipe> PYROLYSE_OVEN = REGISTRATE.recipeType(
+            "pyrolyse_oven",
+            PyrolyseOvenRecipe.class,
+            PyrolyseOvenRecipe.Serializer::new
+    ).register();
 
     public static void init() {
 
--- src/main/java/com/chen1335/immersiveMechanical/recipe/PyrolyseOvenRecipe.java
+++ src/main/java/com/chen1335/immersiveMechanical/recipe/PyrolyseOvenRecipe.java
@@ -1,25 +1,69 @@
 package com.chen1335.immersiveMechanical.recipe;
 
-import blusunrize.immersiveengineering.api.crafting.IERecipeSerializer;
-import blusunrize.immersiveengineering.api.crafting.IERecipeTypes;
-import blusunrize.immersiveengineering.api.crafting.MultiblockRecipe;
-import blusunrize.immersiveengineering.api.crafting.TagOutput;
+import blusunrize.immersiveengineering.api.crafting.*;
+import blusunrize.immersiveengineering.common.register.IEFluids;
+import com.chen1335.immersiveMechanical.definitions.IMRecipe;
+import com.google.common.base.Suppliers;
+import malte0811.dualcodecs.DualMapCodec;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.neoforged.neoforge.fluids.FluidStack;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class PyrolyseOvenRecipe extends MultiblockRecipe {
-    protected <T extends Recipe<?>> PyrolyseOvenRecipe(TagOutput outputDummy, IERecipeTypes.TypeWithClass<T> type, int baseTime, int baseEnergy, Supplier<RecipeMultiplier> multipliers) {
-        super(outputDummy, type, baseTime, baseEnergy, multipliers);
+    public static final Supplier<RecipeMultiplier> MULTIPLIERS = Suppliers.memoize(() -> new RecipeMultiplier(PyrolyseOvenRecipe::timeModifier, PyrolyseOvenRecipe::energyModifier));
+    private final IngredientWithSize input;
+    private final List<StackWithChance> outPutsWithChance;
+    private final FluidStack fluidOutput;
+
+    private static double energyModifier() {
+        return 1;
+    }
+
+    private static double timeModifier() {
+        return 1;
+    }
+
+    protected <T extends Recipe<?>> PyrolyseOvenRecipe(IngredientWithSize input,
+                                                       TagOutput main,
+                                                       List<StackWithChance> outPutsWithChance,
+                                                       FluidStack fluidOutput,
+                                                       int baseTime, int baseEnergy
+    ) {
+        super(main, IMRecipe.PYROLYSE_OVEN.getIEType(), baseTime, baseEnergy, MULTIPLIERS);
+        this.input = input;
+        this.outPutsWithChance = outPutsWithChance;
+        this.fluidOutput = fluidOutput;
     }
 
     @Override
     protected IERecipeSerializer<?> getIESerializer() {
-        return null;
+        return IMRecipe.PYROLYSE_OVEN.getSerializer();
     }
 
     @Override
     public int getMultipleProcessTicks() {
         return 0;
+    }
+
+    public static PyrolyseOvenRecipe fromCokeOvenRecipe(CokeOvenRecipe recipe) {
+        return new PyrolyseOvenRecipe(recipe.input, recipe.output, List.of(), new FluidStack(IEFluids.CREOSOTE.getStill(), recipe.creosoteOutput), recipe.time / 4, 25600);
+    }
+
+    public static class Serializer extends IERecipeSerializer<PyrolyseOvenRecipe> implements RecipeSerializer<PyrolyseOvenRecipe> {
+
+        @Override
+        public ItemStack getIcon() {
+            return null;
+        }
+
+        @Override
+        protected DualMapCodec<RegistryFriendlyByteBuf, PyrolyseOvenRecipe> codecs() {
+            return null;
+        }
     }
 }
