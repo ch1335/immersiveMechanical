5362134b88a27eff9a948fc656bbaec5c63bff4d
chen_1335 <3388508783@qq.com>
2026-07-12 15:18:14 +0800

    热解炉jei

--- src/main/java/com/chen1335/immersiveMechanical/compat/jei/IMJei.java
+++ src/main/java/com/chen1335/immersiveMechanical/compat/jei/IMJei.java
@@ -9,9 +9,11 @@ import com.chen1335.immersiveMechanical.ImmersiveMechanical;
 import com.chen1335.immersiveMechanical.compat.jei.categories.GeneratorFuelCategory;
 import com.chen1335.immersiveMechanical.compat.jei.categories.IndustrialFurnaceCategory;
 import com.chen1335.immersiveMechanical.compat.jei.categories.MineralMixCategory;
+import com.chen1335.immersiveMechanical.compat.jei.categories.PyrolyseOvenCategory;
 import com.chen1335.immersiveMechanical.definitions.IMMultiblocks;
 import com.chen1335.immersiveMechanical.definitions.IMRecipe;
 import com.chen1335.immersiveMechanical.recipe.IndustrialFurnaceRecipe;
+import com.chen1335.immersiveMechanical.recipe.PyrolyseOvenRecipe;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.helpers.IJeiHelpers;
@@ -31,6 +33,8 @@ public class IMJei implements IModPlugin {
 
     public static final RecipeType<RecipeHolder<IndustrialFurnaceRecipe>> INDUSTRIAL_FURNACE = RecipeType.createRecipeHolderType(IMRecipe.INDUSTRIAL_FURNACE.getId());
 
+    public static final RecipeType<RecipeHolder<PyrolyseOvenRecipe>> PYROLYSE_OVEN = RecipeType.createRecipeHolderType(IMRecipe.PYROLYSE_OVEN.getId());
+
     public static final RecipeType<RecipeHolder<GeneratorFuel>> GENERATOR_FUEL = RecipeType.createRecipeHolderType(IERecipeTypes.GENERATOR_FUEL.type().getId());
 
     public static final RecipeType<RecipeHolder<MineralMix>> MINERAL_MIX = RecipeType.createRecipeHolderType(IERecipeTypes.MINERAL_MIX.type().getId());
@@ -44,6 +48,7 @@ public class IMJei implements IModPlugin {
     public void registerCategories(IRecipeCategoryRegistration registration) {
         IJeiHelpers jeiHelpers = registration.getJeiHelpers();
         registration.addRecipeCategories(new IndustrialFurnaceCategory(jeiHelpers));
+        registration.addRecipeCategories(new PyrolyseOvenCategory(jeiHelpers));
         registration.addRecipeCategories(new GeneratorFuelCategory(jeiHelpers));
         registration.addRecipeCategories(new MineralMixCategory(jeiHelpers));
     }
@@ -54,8 +59,8 @@ public class IMJei implements IModPlugin {
         registration.addRecipeCatalyst(greenHouseIconStack, JEIRecipeTypes.CLOCHE);
         registration.addRecipeCatalyst(greenHouseIconStack, JEIRecipeTypes.CLOCHE_FERTILIZER);
         registration.addRecipeCatalyst(IMMultiblocks.INDUSTRIAL_FURNACES.registration().iconStack(), INDUSTRIAL_FURNACE);
+        registration.addRecipeCatalyst(IMMultiblocks.PYROLYSE_OVEN_DEMO.getBlock().asItem(), PYROLYSE_OVEN);
         registration.addRecipeCatalyst(IEMultiblocks.DIESEL_GENERATOR.getBlock().asItem(), GENERATOR_FUEL);
-        registration.addRecipeCatalyst(IMMultiblocks.PYROLYSE_OVEN.registration().iconStack(), JEIRecipeTypes.COKE_OVEN);
     }
 
     @Override
@@ -63,6 +68,7 @@ public class IMJei implements IModPlugin {
         if (Minecraft.getInstance().level != null) {
             RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
             registration.addRecipes(INDUSTRIAL_FURNACE, recipeManager.getAllRecipesFor(IMRecipe.INDUSTRIAL_FURNACE.get()));
+            registration.addRecipes(PYROLYSE_OVEN, recipeManager.getAllRecipesFor(IMRecipe.PYROLYSE_OVEN.get()));
             registration.addRecipes(GENERATOR_FUEL, recipeManager.getAllRecipesFor(IERecipeTypes.GENERATOR_FUEL.get()));
             registration.addRecipes(MINERAL_MIX, recipeManager.getAllRecipesFor(IERecipeTypes.MINERAL_MIX.get()));
         }
--- src/main/java/com/chen1335/immersiveMechanical/compat/jei/categories/PyrolyseOvenCategory.java
+++ src/main/java/com/chen1335/immersiveMechanical/compat/jei/categories/PyrolyseOvenCategory.java
@@ -1,9 +1,93 @@
 package com.chen1335.immersiveMechanical.compat.jei.categories;
 
+import blusunrize.immersiveengineering.common.util.compat.jei.JEIHelper;
 import com.chen1335.immersiveMechanical.ImmersiveMechanical;
+import com.chen1335.immersiveMechanical.compat.jei.IMJei;
+import com.chen1335.immersiveMechanical.definitions.IMMultiblocks;
+import com.chen1335.immersiveMechanical.recipe.PyrolyseOvenRecipe;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.helpers.IJeiHelpers;
+import mezz.jei.api.neoforge.NeoForgeTypes;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.neoforged.neoforge.fluids.FluidStack;
+import org.jetbrains.annotations.Nullable;
 
-public class PyrolyseOvenCategory {
-    public static final ResourceLocation ICON = ImmersiveMechanical.id("textures/jei/container/pyrolyse_oven_jei.png");
+public class PyrolyseOvenCategory implements IRecipeCategory<RecipeHolder<PyrolyseOvenRecipe>> {
+    public static final ResourceLocation ICON = ImmersiveMechanical.id("textures/gui/jei/pyrolyse_oven_jei.png");
+    private static final Component TITLE = Component.translatable("block.immersive_mechanical.pyrolyse_oven");
 
+    private final IDrawable icon;
+    private final IDrawableStatic background;
+
+    public PyrolyseOvenCategory(IJeiHelpers jeiHelpers) {
+        IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
+        this.background = guiHelper.drawableBuilder(ICON, 0, 0, 123, 60).setTextureSize(123, 60).build();
+        this.icon = guiHelper.createDrawableItemStack(IMMultiblocks.PYROLYSE_OVEN.registration().iconStack());
+    }
+
+    @Override
+    public RecipeType<RecipeHolder<PyrolyseOvenRecipe>> getRecipeType() {
+        return IMJei.PYROLYSE_OVEN;
+    }
+
+    @Override
+    public Component getTitle() {
+        return TITLE;
+    }
+
+    @Override
+    public @Nullable IDrawable getIcon() {
+        return icon;
+    }
+
+    @Override
+    public int getWidth() {
+        return 128;
+    }
+
+    @Override
+    public int getHeight() {
+        return 64;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, RecipeHolder<PyrolyseOvenRecipe> holder, IFocusGroup focuses) {
+        PyrolyseOvenRecipe recipe = holder.value();
+        int batchSize = recipe.getInput().getCount();
+
+        builder.addSlot(RecipeIngredientRole.INPUT, 4, 15)
+                .addItemStacks(recipe.getInput().getMatchingStackList())
+                .addRichTooltipCallback((slot, tooltip) -> tooltip.add(
+                        Component.translatable("desc.immersiveengineering.info.batched", batchSize).withStyle(ChatFormatting.GOLD)
+                ));
+
+        if (!recipe.getOutput().get().isEmpty()) {
+            builder.addSlot(RecipeIngredientRole.OUTPUT, 60, 15).addItemStack(recipe.getOutput().get());
+        }
+
+        FluidStack fluidOutput = recipe.getFluidOutput();
+        if (!fluidOutput.isEmpty()) {
+            builder.addSlot(RecipeIngredientRole.OUTPUT, 103, 7)
+                    .setFluidRenderer(12000L, false, 16, 47)
+                    .addIngredient(NeoForgeTypes.FLUID_STACK, fluidOutput)
+                    .addRichTooltipCallback(JEIHelper.fluidTooltipCallback);
+        }
+    }
+
+    @Override
+    public void draw(RecipeHolder<PyrolyseOvenRecipe> recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+        background.draw(guiGraphics);
+    }
 }
