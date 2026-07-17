dfd4a4d991a8c49328505226deae7e6754d7fdbe
chen_1335 <3388508783@qq.com>
2026-07-17 13:50:02 +0800

    调整

---
+++ src/main/java/com/chen1335/immersiveMechanical/kubejs/IEJSKubeJSPlugin.java
@@ -0,0 +1,17 @@
+package com.chen1335.immersiveMechanical.kubejs;
+
+import com.chen1335.immersiveMechanical.ImmersiveMechanical;
+import com.chen1335.immersiveMechanical.kubejs.recipe.IndustrialFurnaceSchema;
+import com.chen1335.immersiveMechanical.kubejs.recipe.PyrolyseOvenSchema;
+import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
+import dev.latvian.mods.kubejs.recipe.schema.RecipeSchemaRegistry;
+
+public class IEJSKubeJSPlugin implements KubeJSPlugin {
+    public static final IEJSKubeJSPlugin INSTANCE = new IEJSKubeJSPlugin();
+
+    @Override
+    public void registerRecipeSchemas(RecipeSchemaRegistry registry) {
+        registry.register(ImmersiveMechanical.id("industrial_furnace"), IndustrialFurnaceSchema.SCHEMA);
+        registry.register(ImmersiveMechanical.id("pyrolyse_oven"), PyrolyseOvenSchema.SCHEMA);
+    }
+}
--- src/main/java/com/chen1335/immersiveMechanical/kubejs/IMKubeJSPlugin.java
+++ src/main/java/com/chen1335/immersiveMechanical/kubejs/IMKubeJSPlugin.java
@@ -1,15 +1,14 @@
 package com.chen1335.immersiveMechanical.kubejs;
 
-import com.chen1335.immersiveMechanical.ImmersiveMechanical;
-import com.chen1335.immersiveMechanical.kubejs.recipe.IndustrialFurnaceSchema;
-import com.chen1335.immersiveMechanical.kubejs.recipe.PyrolyseOvenSchema;
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchemaRegistry;
+import net.neoforged.fml.ModList;
 
 public class IMKubeJSPlugin implements KubeJSPlugin {
     @Override
     public void registerRecipeSchemas(RecipeSchemaRegistry registry) {
-        registry.register(ImmersiveMechanical.id("industrial_furnace"), IndustrialFurnaceSchema.SCHEMA);
-        registry.register(ImmersiveMechanical.id("pyrolyse_oven"), PyrolyseOvenSchema.SCHEMA);
+        if (ModList.get().isLoaded("immersive_engineering_js")) {
+            IEJSKubeJSPlugin.INSTANCE.registerRecipeSchemas(registry);
+        }
     }
 }
